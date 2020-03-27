7aa7f916094d53f83516413f99070d0a2a9d9dd6
PW <przemek.wrobel@poczta.fm>
2020-03-27 11:41:00 +0100

    Initial

---
+++ Kolekcje/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolekcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] montsOfYear =
+            {
+                "January",
+                "February",
+                "March",
+                "April",
+                "Maj",
+                "June",
+                "Julay",
+                "August",
+                "September",
+                "October",
+                "November",
+                "Desember",
+
+            };
+        }
+    }
+}
